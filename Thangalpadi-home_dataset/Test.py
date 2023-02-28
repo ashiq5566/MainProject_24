a7f723493aa8190a917aa120a6dc78b68682ac13
mohammedashiq3344 <ashiq5566.k@gmail.com>
2023-02-28 11:54:12 +0530

    first commit

---
+++ Thangalpadi-home_dataset/Test.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+myData = pd.read_csv('/home/ashiq/Documents/MCA/s4/Project/Thangalpadi-home_dataset/dataset1.csv')
+print(myData.iloc[:, 0])
